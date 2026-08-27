13b098cfbacf76bf99dee1f90c0536d531694089
tomasEW <164850168+tomasEW@users.noreply.github.com>
2026-08-27 14:56:18 +0800

    feat: expose dedicated mode-toggle hotkey in settings

---
+++ src/views/CustomSettingsView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import SettingsView from "./SettingsView.vue";
+import ModeToggleHotkeySetting from "../components/ModeToggleHotkeySetting.vue";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+</script>
+
+<template>
+  <div>
+    <div class="px-6 pt-6">
+      <Card>
+        <CardHeader class="border-b border-border">
+          <CardTitle class="text-base">SayIt Custom 快捷鍵</CardTitle>
+        </CardHeader>
+        <CardContent class="pt-4">
+          <ModeToggleHotkeySetting />
+        </CardContent>
+      </Card>
+    </div>
+    <SettingsView />
+  </div>
+</template>
